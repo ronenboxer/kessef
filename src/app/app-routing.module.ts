fb2302ef41d68f3da0b5aae8b354a1839ebc4a77
Ronen Boxer <ronenboxer@gmail.com>
2023-01-03 22:17:43 +0200

    first deployment try

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,73 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { ContactResolver } from './services/contact/contact.resolver';
+import { ContactDetailsComponent } from './views/contact-details/contact-details.component';
+import { ContactEditComponent } from './views/contact-edit/contact-edit.component';
+import { ContactPageComponent } from './views/contact-page/contact-page.component';
+import { HomePageComponent } from './views/home-page/home-page.component';
+import { StatisticsPageComponent } from './views/statistics-page/statistics-page.component';
+import { TransactionsComponent } from './views/transactions/transactions.component';
+import { UserSignComponent } from './views/user-sign/user-sign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'statistics',
+    component: StatisticsPageComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'edit/:id',
+        component: ContactEditComponent,
+        resolve: { contact: ContactResolver },
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'edit',
+        component: ContactEditComponent,
+        resolve: { contact: ContactResolver },
+        canActivate: [AuthGuard]
+      }
+    ]
+  },
+  {
+    path: 'contact/:id',
+    component: ContactDetailsComponent,
+    resolve: { contact: ContactResolver },
+    canActivate: [AuthGuard],
+
+    children: [
+      {
+        path: 'edit',
+        component: ContactEditComponent,
+        resolve: { contact: ContactResolver },
+        canActivate: [AuthGuard]
+      }
+    ]
+  },
+  {
+    path: 'transactions',
+    component: TransactionsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'sign',
+    component: UserSignComponent
+  },
+  {
+    path: '',
+    component: HomePageComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true, paramsInheritanceStrategy: 'always' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-root/app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { HomePageComponent } from './views/home-page/home-page.component';
+import { ContactPageComponent } from './views/contact-page/contact-page.component';
+import { ContactPreviewComponent } from './cmps/contact-preview/contact-preview.component';
+import { ContactListComponent } from './cmps/contact-list/contact-list.component';
+import { AppIndexComponent } from './views/app-index/app-index.component';
+import { StatisticsPageComponent } from './views/statistics-page/statistics-page.component';
+import { ChartPreviewComponent } from './cmps/chart-preview/chart-preview.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ContactDetailsComponent } from './views/contact-details/contact-details.component';
+import { ContactEditComponent } from './views/contact-edit/contact-edit.component';
+import { UserSignComponent } from './views/user-sign/user-sign.component';
+import { HeaderComponent } from './cmps/header/header.component';
+import { TransactionsComponent } from './views/transactions/transactions.component';
+import { TransactionListComponent } from './cmps/transaction-list/transaction-list.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ContactPageComponent,
+    ContactPreviewComponent,
+    ContactListComponent,
+    AppIndexComponent,
+    StatisticsPageComponent,
+    ChartPreviewComponent,
+    ContactDetailsComponent,
+    ContactEditComponent,
+    UserSignComponent,
+    HeaderComponent,
+    TransactionsComponent,
+    TransactionListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
